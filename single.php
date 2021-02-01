1cf30d68c6254c35486bb5c6a7b184dbdc14e6dd
Andrei Racasan <aracasan@outlook.com>
2021-02-01 22:07:07 +0000

    create archive, posts

--- single.php
+++ single.php
@@ -2,19 +2,19 @@
 
 <article class="content px-3 py-5 p-md-5">
 
+  <?php
+
+  if (have_posts()) {
+    while (have_posts()) {
+      the_post();
+
+      get_template_part('template_parts/content', 'article');
+    }
+  };
+
+  ?>
+
 </article>
-
-<?php
-
-if (have_posts()) {
-  while (have_posts()) {
-    the_post();
-
-    get_template_part('template_parts/content', 'article');
-  }
-};
-
-?>
 
 
 </div>
