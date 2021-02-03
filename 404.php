224fab3ce57c3b4175c2ec1bd8fe415989a2974d
Andrei Racasan <aracasan@outlook.com>
2021-02-03 22:07:26 +0000

    add page, search, 404

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<article class="content px-3 py-5 p-md-5">
+
+  <h1>Page not found</h1>
+
+  <?php
+  get_search_form();
+  ?>
+
+</article>
+
+</div>
+
+<?php get_footer(); ?>
